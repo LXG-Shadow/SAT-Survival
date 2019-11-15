ca3d1c88f404737975e60ffef351258712415bbf
aynakeya <670379848@qq.com>
2019-11-15 16:44:17 +0800

    update

--- CustomItem/src/main/java/com/lxgshadow/customitem/ArkNightsCollection/ChiXiao.java
+++ CustomItem/src/main/java/com/lxgshadow/customitem/ArkNightsCollection/ChiXiao.java
@@ -1,20 +1,30 @@
 package com.lxgshadow.customitem.ArkNightsCollection;
 
+import com.google.gson.internal.$Gson$Types;
 import com.lxgshadow.customitem.Main;
+import com.lxgshadow.customitem.utils.IgnoreSelf;
+import com.lxgshadow.customitem.utils.ItemUtils;
 import net.minecraft.server.v1_14_R1.NBTTagCompound;
 import net.minecraft.server.v1_14_R1.NBTTagInt;
 import net.minecraft.server.v1_14_R1.NBTTagList;
 import net.minecraft.server.v1_14_R1.NBTTagString;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.*;
 import org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class ChiXiao {
     private static ItemStack item;
@@ -37,7 +47,7 @@ public class ChiXiao {
         meta.setDisplayName(dpName);
         meta.setLore(new ArrayList<>(Arrays.asList(lores)));
         item.setItemMeta(meta);
-        //Main.getInstance().getServer().getPluginManager().registerEvents(new LumbererAxeListener(),Main.getInstance());
+        Main.getInstance().getServer().getPluginManager().registerEvents(new ChiXiaoListener(),Main.getInstance());
     }
 
     // from https://bukkit.org/threads/how-do-i-add-attributes-to-a-itemstack.404299/
@@ -80,9 +90,109 @@ public class ChiXiao {
         return item;
     }
 }
+class ChiXiaoListener implements Listener{
+    HashMap<UUID, List<Integer>> trigger;
+    public ChiXiaoListener(){
+        trigger = new HashMap<>();
+    }
+    @EventHandler
+    public void onClick(PlayerInteractEvent event){
+        Player player = event.getPlayer();
+        PlayerInventory inv = player.getInventory();
+        if (!ItemUtils.isRegisterNameSimilar(ChiXiao.regName,inv.getItemInMainHand())){return;}
+        if (!trigger.containsKey(player.getUniqueId())){
+            trigger.put(player.getUniqueId(),new ArrayList<>());
+        }
+        int val = -1;
+        // left 0 right 1
+        if (event.getAction() == Action.LEFT_CLICK_AIR) {
+            val = 0;
+        }else if (event.getAction() == Action.RIGHT_CLICK_AIR){
+            val = 1;
+        }else {return;}
+        if (trigger.get(player.getUniqueId()).size() == 0){
+            new BukkitRunnable(){
+                @Override
+                public void run() {
+                    trigger.get(player.getUniqueId()).clear();
+                }
+            }.runTaskLater(Main.getInstance(),2*20);
+        }
+        trigger.get(player.getUniqueId()).add(val);
+        if (trigger.get(player.getUniqueId()).size() == 3){
+            ChiXiaoSkills.choose(player,
+                    trigger.get(player.getUniqueId()).get(0),
+                    trigger.get(player.getUniqueId()).get(1),
+                    trigger.get(player.getUniqueId()).get(2));
+        }
+    }
+
+}
+
 
 class ChiXiaoSkills{
-    public static void BaDao(){
-
+    public static void choose(Player player,int s1,int s2,int s3){
+        if (s1 == 1 && s2 == 1 && s3 == 0){
+            BaDao(player);
+        }
+    }
+    public static void BaDao(Player player){
+        Location location = player.getLocation();
+        Location eyeLocation = player.getEyeLocation();
+        Vector direction = eyeLocation.getDirection();
+        player.sendMessage(eyeLocation.getYaw()+"");
+        double yaw = eyeLocation.getYaw();
+        if (yaw < 0){
+            yaw += 360;
+        }
+        // Yaw
+        // positive z 0
+        // negative z 180
+        // positive x 270
+        // negative x 90
+        Vector front,left,right;
+        if (yaw >= 315 || yaw < 45){
+            front = new Vector(0,0,1);
+            left = new Vector(1,0,0);
+            right = new Vector(-1,0,0);
+        }
+        else if (yaw >= 45 && yaw < 135){
+            front = new Vector(-1,0,0);
+            left = new Vector(0,0,1);
+            right = new Vector(0,0,-1);
+        }
+        else if (yaw >= 135 && yaw < 225){
+            front = new Vector(0,0,-1);
+            left = new Vector(-1,0,0);
+            right = new Vector(1,0,0);
+        }
+        //if (yaw >= -135 && yaw <-45){
+        else {
+            front = new Vector(1,0,0);
+            left = new Vector(0,0,-1);
+            right = new Vector(0,0,1);
+        }
+        // lift up to the middle of player's height
+        location.add(0,0.8,0);
+        // front
+        for (double i=0.2;i<=6;i+=0.2){
+            // particle effect
+            for (int j=0;j<2;j++){
+                player.getWorld().spawnParticle(Particle.REDSTONE,location.clone().add(front.clone().multiply(i)),
+                        0,2,2,2,new Particle.DustOptions(Color.RED,2F));
+            }
+        }
+        HashSet<LivingEntity> livingEntities = new HashSet<>();
+        for (int i=0;i<=6;i++){
+            for (Entity entity:player.getWorld().getNearbyEntities(location.clone().add(front.clone().multiply(i)),
+                    1,1,1,new IgnoreSelf(player))){
+                if (entity instanceof LivingEntity){
+                    livingEntities.add((LivingEntity) entity);
+                }
+            }
+        }
+        for (LivingEntity entity:livingEntities){
+            entity.damage(1024,player);
+        }
     }
 }
