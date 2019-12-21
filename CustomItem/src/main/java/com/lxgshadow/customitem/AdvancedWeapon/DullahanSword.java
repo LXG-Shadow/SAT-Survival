d74943bd0625efb3ab49e9fdeb2a493a417111a1
aynakeya <670379848@qq.com>
2019-12-21 22:05:51 +0800

    update

---
+++ CustomItem/src/main/java/com/lxgshadow/customitem/AdvancedWeapon/DullahanSword.java
@@ -0,0 +1,54 @@
+package com.lxgshadow.customitem.AdvancedWeapon;
+
+import com.lxgshadow.customitem.AdvancedTool.LumbererAxeListener;
+import com.lxgshadow.customitem.Main;
+import com.lxgshadow.customitem.interfaces.CustomItems;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DullahanSword implements CustomItems {
+    private static ItemStack item;
+    private static String name = "Dullahan Sword",
+    private static String dpName = ChatColor.AQUA+"Dullahan's Sword";
+    public static String regName = name.replace(" ","_").toLowerCase();
+    private static String[] lores = {
+            ChatColor.AQUA+""+ChatColor.ITALIC+"Advanced Weapon",
+            ChatColor.WHITE+""+ChatColor.ITALIC+"Description: Where is my head?",
+            ChatColor.WHITE+""+ChatColor.ITALIC+"*Player drop head when killed by this sword",
+            ChatColor.BLACK+"Register Name: ["+regName+"]"
+    };
+
+    public static ItemStack getItem() {
+        return item;
+    }
+
+    public static void createRecipe(){
+        item = new ItemStack(Material.IRON_SWORD);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(dpName);
+        meta.setLore(new ArrayList<>(Arrays.asList(lores)));
+        item.setItemMeta(meta);
+        ShapedRecipe r = new ShapedRecipe(new NamespacedKey(Main.getInstance(),regName),item);
+        r.shape("101","101","101");
+        r.setIngredient('0',Material.IRON_SWORD);
+        r.setIngredient('1',Material.REDSTONE_BLOCK);
+        Main.getInstance().getServer().addRecipe(r);
+        Main.getInstance().getServer().getPluginManager().registerEvents(new DullahanSwordListener(),Main.getInstance());
+    }
+}
+class DullahanSwordListener implements Listener{
+    @EventHandler
+    public void ondead(PlayerDeathEvent event){
+        if (event.getEntity().getKiller()){}
+    }
+}
