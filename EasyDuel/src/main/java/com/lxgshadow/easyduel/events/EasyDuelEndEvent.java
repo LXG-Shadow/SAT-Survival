7397c47c187da3c8d142303990af5a114b0fe0b7
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2020-05-23 22:55:33 +0800

    rewrite easyduel

--- EasyDuel/src/main/java/com/lxgshadow/easyduel/events/EasyDuelEndEvent.java
+++ EasyDuel/src/main/java/com/lxgshadow/easyduel/events/EasyDuelEndEvent.java
@@ -16,7 +16,6 @@ public class EasyDuelEndEvent extends EasyDuelEvent{
         return arena;
     }
 
-
     public ArenaTeam getWinnerTeam(){
         for (ArenaTeam at:arena.getTeams()){
             if (at.isWin()){
